abbc1707cf3a3d12424b191dcc92565fe10083a3
Damian1457 <damianwasik.coach@gmail.com>
2022-09-13 21:07:10 +0200

    Add heroku configuration

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,13 +2,29 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication {
+//public class TasksApplication extends SpringBootServletInitializer {
 
+
+public class TaskApplication {
 	public static void main(String[] args) {
 
-		SpringApplication.run(TasksApplication.class, args);
+		SpringApplication.run(TaskApplication.class, args);
 	}
 
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		//return application.sources(TasksApplication.class);
+	//}
+
 }
+
+// 1. Postman,
+// 2. Metoda Delete w zadaniu - aplikacja,
+// 3. Tomcat,
+// 4. Widok w aplikacji, nie wyświetla się jedna część,
+// 5. Nie mogę wysłać kodu na githuba,
+// 6. Nie działa mi strona aplikacji.
