61decba807deed9b7ca6288351d1c363463880f7
Damian1457 <damianwasik.coach@gmail.com>
2022-09-14 18:40:29 +0200

    Add heroku configuration

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,17 +2,15 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+
 //public class TasksApplication extends SpringBootServletInitializer {
 
-
-public class TaskApplication {
+@SpringBootApplication
+public class TasksApplication {
 	public static void main(String[] args) {
 
-		SpringApplication.run(TaskApplication.class, args);
+		SpringApplication.run(TasksApplication.class, args);
 	}
 
 	//@Override
@@ -27,4 +25,5 @@ public class TaskApplication {
 // 3. Tomcat,
 // 4. Widok w aplikacji, nie wyświetla się jedna część,
 // 5. Nie mogę wysłać kodu na githuba,
-// 6. Nie działa mi strona aplikacji.
+// 6. Nie działa mi strona aplikacji,
+//7. Heroku.
